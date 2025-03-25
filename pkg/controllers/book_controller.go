07d43f27366d0d7cde6899190043dc2056e73b1f
Gerard-007 <gerardnwazk@gmail.com>
2025-03-25 21:06:21 +0100

    Basic App structure completed

--- pkg/controllers/book_controller.go
+++ pkg/controllers/book_controller.go
@@ -12,7 +12,13 @@ import (
 var NewBook models.Book
 
 func CreateBook(writer http.ResponseWriter, request *http.Request) {
-
+	createBook := &models.Book{}
+	utils.ParseBody(request, createBook)
+	book := createBook.CreateBook()
+	response, _ := json.Marshal(book)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusCreated)
+	writer.Write(response)
 }
 
 func GetBooks(writer http.ResponseWriter, request *http.Request) {
@@ -38,9 +44,41 @@ func GetBookByID(writer http.ResponseWriter, request *http.Request) {
 }
 
 func UpdateBook(writer http.ResponseWriter, request *http.Request) {
-
+	var updateBook = &models.Book{}
+	utils.ParseBody(request, updateBook)
+	vars := mux.Vars(request)
+	bookid := vars["bookid"]
+	ID, err := strconv.ParseInt(bookid, 10, 64)
+	if err != nil {
+		writer.WriteHeader(http.StatusNotFound)
+	}
+	bookDetails, db := models.GetBookById(ID)
+	if updateBook.Title != "" {
+		bookDetails.Title = updateBook.Title
+	}
+	if updateBook.Author != "" {
+		bookDetails.Author = updateBook.Author
+	}
+	if updateBook.Publication != "" {
+		bookDetails.Publication = updateBook.Publication
+	}
+	db.Save(&bookDetails)
+	response, _ := json.Marshal(bookDetails)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(response)
 }
 
 func DeleteBook(writer http.ResponseWriter, request *http.Request) {
-
+	vars := mux.Vars(request)
+	bookid := vars["bookid"]
+	ID, err := strconv.ParseInt(bookid, 10, 64)
+	if err != nil {
+		writer.WriteHeader(http.StatusNotFound)
+	}
+	book := models.DeleteBook(ID)
+	response, _ := json.Marshal(book)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(response)
 }
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func ParseBody(resp *http.Response, x interface{}) error {
+func ParseBody(resp *http.Request, x interface{}) error {
 	if body, err := io.ReadAll(resp.Body); err == nil {
 		if err := json.Unmarshal(body, x); err != nil {
 			return fmt.Errorf("failed to unmarshal JSON: %w", err)
